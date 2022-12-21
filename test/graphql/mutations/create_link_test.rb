92a9153f12e258004db37d8124117102c61a7a05
ayrton <ayrton.parkinson1@gmail.com>
2022-12-21 07:55:24 -0500

    Fix failing test

--- test/graphql/mutations/create_link_test.rb
+++ test/graphql/mutations/create_link_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class Mutations::CreateLinkTest < ActiveSupport::TestCase
+  def create_user
+    User.create(name: 'test', email: 'test@example.com', password: '123456')
+  end
+
   def perform(user: nil, **args)
-    Mutations::CreateLink.new(object: nil, field: nil, context: {}).resolve(**args)
+    Mutations::CreateLink.new(object: nil, field: nil, context: { current_user: create_user }).resolve(**args)
   end
 
   test 'create a new link' do
     link = perform(
       url: 'http://example.com',
-      description: 'description'
+      description: 'description',
+      user: create_user
     )
 
     assert link.persisted?
